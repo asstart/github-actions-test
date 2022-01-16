fa0bddc5f25d0130370fee08822065334f422a26
asstart <asst.artem@gmail.com>
2022-01-16 23:23:07 +0300

    Fix test_secret.sh

--- .github/scripts/test_secret.sh
+++ .github/scripts/test_secret.sh
@@ -2,9 +2,12 @@
 
 echo "Testing secret validity"
 
+SECRET_LEN=${#SUPER_SECRET}
+
 if [[ -z $SUPER_SECRET ]]; then
     echo "Oh, secret is emtpy"
-elif [[ ${#SUPER_SECRET} lt 5 ]]; then
+elif [[ ${#SUPER_SECRET} -lt 5 ]]; then
     echo "Oh, secret is too short, probably it's wrong"
 else
     echo "Secret is ok, we can go further"
+fi
