ee681c790d71a87e9796dbcf509f189714778bb9
asstart <asst.artem@gmail.com>
2022-01-16 23:07:19 +0300

    Update test_secret.sh

--- .github/scripts/test_secret.sh
+++ .github/scripts/test_secret.sh
@@ -2,7 +2,9 @@
 
 echo "Testing secret validity"
 
-if [[ ${#SUPER_SECRET} lt 5 ]]; then
-    echo "Oh, secrets is too short, probably it's wrong"
+if [[ -z $SUPER_SECRET ]]; then
+    echo "Oh, secret is emtpy"
+elif [[ ${#SUPER_SECRET} lt 5 ]]; then
+    echo "Oh, secret is too short, probably it's wrong"
 else
     echo "Secret is ok, we can go further"
